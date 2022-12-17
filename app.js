916bac959f2dbe24964fcecc613ef4e4899f64f7
Tom O'Donovan <tom.odonovan01@gmail.com>
2022-12-17 17:06:54 +1100

    Added test route

--- app.js
+++ app.js
@@ -3,3 +3,19 @@ const express = require("express")
 const bodyParser = require("body-parser")
 
 const app = express();
+
+const PORT = 3000;
+
+app.use(bodyParser.json())
+app.use(express.static('static'))
+
+// Build a basic express app with 1 endpoint /api/hello returns 200, { message: “Hello” }
+
+app.get('/api/hello', (req, res) => {
+    res.status(200).json({'message': "Hello"})
+}) 
+
+
+app.listen(PORT, () => {
+    console.log(`Listening at localhost:${PORT}`)
+})     

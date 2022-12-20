6d9a401d5e64735c64ee0b3c1b0f549712300a6e
Tom O'Donovan <tom.odonovan01@gmail.com>
2022-12-20 14:37:56 +1100

    Updated schema.sql with db tables

---
+++ db/SQL/schema.sql
@@ -0,0 +1,39 @@
+-- Prevents table duplicates
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS calendars;
+DROP TABLE IF EXISTS events;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    email TEXT,
+    password_hash TEXT
+);
+
+CREATE TABLE calendars (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER,
+    name TEXT,
+    description TEXT,
+
+    FOREIGN KEY (user_id)
+        REFERENCES users(id)
+);
+
+CREATE TABLE events (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER,
+    calendar_id INTEGER,
+    title TEXT,
+    date DATE,
+    start_time TIME,
+    end_time TIME,
+    location TEXT,
+    description TEXT,
+
+    FOREIGN KEY (user_id)
+        REFERENCES users(id)
+    
+    FOREIGN KEY (calendar_id)
+        REFERENCES calendars(id)
+);

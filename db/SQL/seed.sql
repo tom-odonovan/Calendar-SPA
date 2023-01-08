67bffdd3a0f50f94cb4edbd3d23a0640c63efaa1
Tom O'Donovan <tom.odonovan01@gmail.com>
2023-01-08 20:02:38 +1100

    Add test events to seed.sql file

---
+++ db/SQL/seed.sql
@@ -0,0 +1,24 @@
+-- Deletes table contents but not the table itself
+TRUNCATE TABLE users;
+TRUNCATE TABLE calendars;
+TRUNCATE TABLE events;
+
+INSERT INTO users (id, name, email, password_hash)
+    VALUES (1, 'Tom', 'tom@example.com');
+
+
+
+INSERT INTO calendars (user_id, name, description) 
+    VALUES (1, 'Calendar 1', 'Example calendar');
+
+
+-- Add some test events into database 
+
+INSERT INTO events 
+    VALUES (1, 1, 1, 'Dinner Party', '1/25/2023', '19:00', '21:00', 'Harry''s');
+
+INSERT INTO events 
+    VALUES (2, 1, 1, 'Physio Appointment', '1/18/2023', '12:00', '12:45', '2/2 Wattle Rd, Brookvale NSW 2100', 'Check in with Steph at the Recentre');
+
+INSERT INTO events 
+    VALUES (3, 1, 1, 'Project 3 Presentaions', '1/14/2023', '12:00', '17:00', 'Online', 'Deadline for Project 3. Meeting link: https://generalassembly.zoom.us/j/93240625082?pwd=MGFraDZKSzFFRnpxakt4emZrQ3VyQT09');

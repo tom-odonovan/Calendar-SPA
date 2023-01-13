ca03a65d3ffe5e2d0d7e66148d441bd663ebd246
Jordan <Jordanshaqallen@gmail.com>
2023-01-13 14:22:13 +0800

    Daily events DELET/EDIT btn

--- static/js/components/render_daily.js
+++ static/js/components/render_daily.js
@@ -17,10 +17,10 @@ export function renderCurrentDay(d) {
     let todayDate = today.getDate()
     let todayMonth = today.toLocaleDateString('default', { month: 'long' })
     let todayYear = today.getFullYear()
-    
+
 
     //Generating parent flex div
-    
+
     const dayContainer = document.createElement('div')
     dayContainer.setAttribute('id', 'dayContainer')
     calendar.append(dayContainer)
@@ -33,18 +33,18 @@ export function renderCurrentDay(d) {
 
     //NAV ARROWS
     let navArrows = document.createElement('div')
-            navArrows.className = 'nav-arrows'
-            titleDiv.appendChild(navArrows)
+    navArrows.className = 'nav-arrows'
+    titleDiv.appendChild(navArrows)
 
-                let prevMonthBtn = document.createElement('button')
-                prevMonthBtn.id = 'prev-Month'
-                prevMonthBtn.innerText = '←'
-                navArrows.appendChild(prevMonthBtn)
+    let prevMonthBtn = document.createElement('button')
+    prevMonthBtn.id = 'prev-Month'
+    prevMonthBtn.innerText = '←'
+    navArrows.appendChild(prevMonthBtn)
 
-                let nextMonthBtn = document.createElement('button')
-                nextMonthBtn.id = 'next-Month'
-                nextMonthBtn.innerText = '→'
-                navArrows.appendChild(nextMonthBtn)
+    let nextMonthBtn = document.createElement('button')
+    nextMonthBtn.id = 'next-Month'
+    nextMonthBtn.innerText = '→'
+    navArrows.appendChild(nextMonthBtn)
 
     // Allow user to toggle between months using nav arrows
     prevMonthBtn.addEventListener('click', () => {
@@ -59,10 +59,16 @@ export function renderCurrentDay(d) {
     daySection.setAttribute('id', 'daySection')
     dayContainer.append(daySection)
 
+    
+
     const dayTitle = document.createElement('h3')
     dayTitle.innerText = `${todayName} ${todayDate}`
     dayTitle.classList.add('dayTitle')
+    dayTitle.id = 'dayTitle'
     daySection.append(dayTitle)
+
+    
+
 
     //AM START
     const twelveAM = document.createElement('div')
@@ -94,7 +100,8 @@ export function renderCurrentDay(d) {
             const session = response.data
             const user_id = session.user_id
 
-            axios
+            if(session) {
+                axios
                     .get(`http://localhost:3000/api/events/${user_id}`) // <--- INSERT USER-ID FROM SESSION HERE
                     .then((response) => {
                         let events = response.data
@@ -109,89 +116,111 @@ export function renderCurrentDay(d) {
                             const day = date.getDate()
                             const eventId = event.id
                             const eventSTime = event['start_time']
-                            const eventHour = eventSTime.slice(0,2)
+                            const eventHour = eventSTime.slice(0, 2)
                             const eventDesc = event['description']
                             const eventETime = parseInt(event.end_time)
                             const startTime = parseInt(event.start_time)
                             const eventLoc = event['locaiton']
                             console.log(day)
 
-                            const axis = 1
+                            
 
-                            if(day === todayDate){
+                            if (day === todayDate) {
                                 //EVENT DIV 
-                                const divSize = ((eventETime - startTime)*50 +25)
-                                console.log(divSize)
+                                
+                                const divSize = ((eventETime - startTime) * 50 + 25)
                                 const hourElement = document.getElementById(`${eventHour}`)
                                 const eventCont = document.createElement('div')
                                 eventCont.id = `event${event.id}`
-                                eventCont.classList.add('eventDay',`${event.id}`)
-                                eventCont.innerHTML = `${event.title}`
+                                eventCont.classList.add('eventDay', `${event.id}`)
                                 hourElement.append(eventCont)
 
-                                const specificEvent = document.getElementById(`event${event.id}`)
-                                specificEvent.style.height = `${divSize}px`
+                                
 
+                                const specificEvent = document.getElementById(`event${event.id}`)
+                                const eventTitle = document.createElement('div')
+                                eventTitle.id = 'details'
+                                eventTitle.innerHTML = `<p><strong>${event.title}</p>`
+                                specificEvent.style.height = `${divSize}px`
+                                specificEvent.style.maxHeight = `${divSize}px`
+                                specificEvent.append(eventTitle)
+
+                                //EVENT DETAILS
+                                const eventDetails = document.createElement('div')
+                                eventDetails.id = 'details'
+                                eventDetails.innerHTML = `
+                                <p>${(event.start_time).slice(0, 5)} - ${(event.end_time).slice(0, 5)}</p>
+                                `
+                                console.log(eventDetails)
+                                eventCont.append(eventDetails)
+
+
+
+
+                                //EDIT-DELETE DIV
                                 const deleteDiv = document.createElement('div')
                                 deleteDiv.classList.add('deleteDiv')
                                 specificEvent.append(deleteDiv)
 
+                                const descDiv = document.createElement('div')
+                                descDiv.innerHTML = `<p>${eventDesc}</p>`
+                                descDiv.id = `desc${event.id}`
+                                // descDiv.classList.add('edit_btn')
+                                deleteDiv.append(descDiv)
+
+                                const editButton = document.createElement('button')
+                                editButton.innerHTML = 'EDIT'
+                                editButton.id = `edit${event.id}`
+                                editButton.classList.add('edit_btn')
+                                deleteDiv.append(editButton)
+
                                 const deleteButton = document.createElement('button')
-                                deleteButton.innerHTML = 'X'
+                                deleteButton.innerHTML = 'DELETE'
                                 deleteButton.id = `del${event.id}`
                                 deleteButton.classList.add('del_btn')
                                 deleteDiv.append(deleteButton)
-                                
-                                
-                                
-                                //EVENT DETAILS
-                                
-                                const eventDetails = document.createElement('div')
-                                eventDetails.id = 'details'
-                                eventDetails.innerHTML=`
-                                <p>${(event.start_time).slice(0,5)} - ${(event.end_time).slice(0,5)}</p>
-                                <p>${eventDesc}</p>
-                                `
-                                
-                                console.log(eventDetails)
-                                eventCont.append(eventDetails)
-                                
-                                const delDay = new Date
-                                const deleteDate = parseInt(day - delDay.getDate())
-                                console.log(day)
-                                console.log(todayDate)
-                                console.log(deleteDate)
-                                const deleteBtn = document.getElementById(`del${event.id}`)
-                                deleteBtn.addEventListener('click', (e) =>{
-                                    
-                                    axios
-                                        .delete(`http://localhost:3000/api/events/${eventId}`)
-                                        .then((response)=>{
-                                            renderCurrentDay(deleteDate)
-                                        })
-                                    
-                                    
-                                    
+
+
+                                //EVENT LISTENER TO SHOW EDIT DIV
+                                specificEvent.addEventListener('mouseenter', e => {
+                                    deleteDiv.style.display = 'flex'
+                                    specificEvent.style.minHeight = "300px"
+                                })
+                                specificEvent.addEventListener('mouseleave', e => {
+                                    deleteDiv.style.display = 'none'
+                                    specificEvent.style.minHeight = ""
                                 })
 
 
 
-                                
+                                //DELETE FUNCTION
+                                const delDay = new Date
+                                const deleteDate = parseInt(day - delDay.getDate())
+                                const deleteBtn = document.getElementById(`del${event.id}`)
+                                deleteBtn.addEventListener('click', (e) => {
+
+                                    axios
+                                        .delete(`http://localhost:3000/api/events/${eventId}`)
+                                        .then((response) => {
+                                            renderCurrentDay(deleteDate)
+                                        })
+                                })
 
                             }
 
-                            
 
-                            
+
+
                         })
                     })
-            })
+            }
+        })
 
 }
 
 
 //WHEN SPECIFIC DAY IS SELECTED
-function renderDay(day, date, month, year){
+function renderDay(day, date, month, year) {
 
     //Generating parent flex div
     const main = document.getElementById('content')
@@ -236,24 +265,24 @@ function renderDay(day, date, month, year){
 
 
 
-function renderHours(prefix){
+function renderHours(prefix) {
     let time = 1
     let hours = 13
     while (time < 12) {
         const hour = document.createElement('div')
         hour.classList.add('hours', `${time}${prefix}`)
-        
-        if(prefix === 'am'){
+
+        if (prefix === 'am') {
             hour.setAttribute('id', `${time}`)
         }
-        if(prefix === 'pm'){
+        if (prefix === 'pm') {
             hour.setAttribute('id', `${hours}`)
             hours++
-        
+
         }
         hour.innerHTML = `<p>${time}${prefix}</p>`
-            daySection.appendChild(hour)
-            time++
-        
+        daySection.appendChild(hour)
+        time++
+
     }
 }
